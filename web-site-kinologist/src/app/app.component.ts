58c532668e902ce8bb65c4cb2fef9ae994e3abfb
Bogdan <tkachovbi@gmail.com>
2022-07-15 22:35:10 +0300

    ninth block and footer

--- web-site-kinologist/src/app/app.component.ts
+++ web-site-kinologist/src/app/app.component.ts
@@ -7,4 +7,31 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'web-site-kinologist';
+  constructor() { }
+
+  ngOnInit(): void { }
+   
+  imgCollection = [
+      {
+        image: 'assets/images/Feedback.png',
+        thumbImage: 'assets/images/Feedback.png',
+        alt: 'Image 1'
+      }, {
+        image: 'assets/images/Feedback2.png',
+        thumbImage: 'assets/images/Feedback2.png',
+        alt: 'Image 2'
+      }, {
+        image: 'assets/images/Feedback3.png',
+        thumbImage: 'assets/images/Feedback3.png',
+        alt: 'Image 3'
+      }, {
+        image: 'assets/images/Feedback4.png',
+        thumbImage: 'assets/images/Feedback4.png',
+        alt: 'Image 4'
+      }, {
+        image: 'assets/images/Feedback5.png',
+        thumbImage: 'assets/images/Feedback5.png',
+        alt: 'Image 5'
+      }
+  ];
 }
--- web-site-kinologist/src/app/app.module.ts
+++ web-site-kinologist/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgImageSliderModule } from 'ng-image-slider';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,6 +10,7 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
+    NgImageSliderModule,
     BrowserModule,
     AppRoutingModule
   ],
