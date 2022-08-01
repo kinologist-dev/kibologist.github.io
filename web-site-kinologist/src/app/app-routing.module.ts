bec65ff3104fc54bc1f0bcd12cb2952a9c1eb3b8
Bogdan <tkachovbi@gmail.com>
2022-08-01 22:31:20 +0300

    PayComponent created and it is works

--- web-site-kinologist/src/app/app-routing.module.ts
+++ web-site-kinologist/src/app/app-routing.module.ts
@@ -4,7 +4,6 @@ import { RouterModule, Routes } from '@angular/router';
 import { CourseComponent } from './components/course/course.component';
 
 const routes: Routes = [
-  {path: '', component: CourseComponent}
 ];
 
 @NgModule({
--- web-site-kinologist/src/app/app.module.ts
+++ web-site-kinologist/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { CourseComponent } from './components/course/course.component';
 import { PayComponent } from './components/pay/pay.component';
 
 const routes: Routes = [
+  {path: 'pay', component: PayComponent},
   {path: '**', component: CourseComponent}
 ];
 
