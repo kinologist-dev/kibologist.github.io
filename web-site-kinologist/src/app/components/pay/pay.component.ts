5fdc7238deef92cb1b36429aa34159f022ea5f60
Bogdan <tkachovbi@gmail.com>
2022-08-08 12:57:03 +0300

    fondy payment added

--- web-site-kinologist/src/app/components/pay/pay.component.ts
+++ web-site-kinologist/src/app/components/pay/pay.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
-import { render } from 'creditcardpayments/creditCardPayments';
 
 @Component({
   selector: 'app-pay',
@@ -10,22 +9,10 @@ import { render } from 'creditcardpayments/creditCardPayments';
 })
 export class PayComponent implements OnInit {
 
-  constructor(private router: Router){
-    
+  constructor(private router: Router) {
   }
 
   ngOnInit(): void {
-    render(
-      
-      {
-        id: "#paypalButtons",
-        currency: "USD",
-        value: "100.00",
-        onApprove: (details) => {
-          window.open('https://t.me/+mJ7S2F8XpbUxZWYy', "_parent")
-        }
-      }
-    );
   }
 
   goToCoursePage = () => {
